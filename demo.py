b17699ff52897a83b35d5dd91396c25c5e60d296
Englert Ervin  Brunó <englert@users.noreply.github.com>
2023-11-29 13:10:04 +0100

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,16 +2,16 @@
 import streamlit as st
  
 # Title
-st.title("Hello GeeksForGeeks !!!")
+st.title(".title: Hello GeeksForGeeks !!!")
 
 # Header
-st.header("This is a header") 
+st.header(".header: This is a header") 
  
 # Subheader
-st.subheader("This is a subheader")
+st.subheader(".subheader: This is a subheader")
 
 # Text
-st.text("Hello GeeksForGeeks!!!")
+st.text(".text: Hello GeeksForGeeks!!!")
 
 # Markdown
 st.markdown("### This is a markdown")
@@ -20,20 +20,20 @@ st.markdown("### This is a markdown")
 st.success("Success")
 
 # success
-st.info("Information")
+st.info(".info: Information")
 
 # success
-st.warning("Warning")
+st.warning(".warning: Warning")
 
 # success
-st.error("Error")
+st.error(".error: Error")
 
 # Exception - This has been added later
 exp = ZeroDivisionError("Trying to divide by Zero")
 st.exception(exp)
 
 # Write text
-st.write("Text with write")
+st.write(".write: Text with write")
 
 # Writing python inbuilt function range()
 st.write(range(10))
@@ -52,70 +52,70 @@ st.write(range(10))
 # checkbox
 # check if the checkbox is checked
 # title of the checkbox is 'Show/Hide'
-if st.checkbox("Show/Hide"):
+if st.checkbox(".checkbox: Show/Hide"):
 
 	# display the text if the checkbox returns True value
-	st.text("Showing the widget")
+	st.text(".text: Showing the widget")
 
 # radio button
 # first argument is the title of the radio button
 # second argument is the options for the radio button
-status = st.radio("Select Gender: ", ('Male', 'Female'))
+status = st.radio(".radio: Select Gender: ", ('Male', 'Female'))
 
 # conditional statement to print 
 # Male if male is selected else print female
 # show the result using the success function
 if (status == 'Male'):
-	st.success("Male")
+	st.success(".success: Male")
 else:
-	st.success("Female")
+	st.success(".success: Female")
 
 # Selection box
 
 # first argument takes the titleof the selectionbox
 # second argument takes options
-hobby = st.selectbox("Hobbies: ",
+hobby = st.selectbox(".selectbox: Hobbies: ",
 					['Dancing', 'Reading', 'Sports'])
 
 # print the selected hobby
-st.write("Your hobby is: ", hobby)
+st.write(".write: Your hobby is: ", hobby)
 
 # multi select box
 
 # first argument takes the box title
 # second argument takes the options to show
-hobbies = st.multiselect("Hobbies: ",
+hobbies = st.multiselect(".multiselect: Hobbies: ",
 						['Dancing', 'Reading', 'Sports'])
 
 # write the selected options
 st.write("You selected", len(hobbies), 'hobbies')
 
 # Create a simple button that does nothing
-st.button("Click me for no reason")
+st.button(".button: Click me for no reason")
 
 # Create a button, that when clicked, shows a text
-if(st.button("About")):
-	st.text("Welcome To GeeksForGeeks!!!")
+if(st.button(".button: About")):
+	st.text(".text: Welcome To GeeksForGeeks!!!")
 
 # Text Input
 
 # save the input text in the variable 'name'
 # first argument shows the title of the text input box
 # second argument displays a default text inside the text input area
-name = st.text_input("Enter Your name", "Type Here ...")
+name = st.text_input(".text_input: Enter Your name", "Type Here ...")
 
 # display the name when the submit button is clicked
 # .title() is used to get the input text string
-if(st.button('Submit')):
+if(st.button('.button: Submit')):
 	result = name.title()
-	st.success(result)
+	st.success(.success: result)
 
 # slider
 
 # first argument takes the title of the slider
 # second argument takes the starting of the slider
 # last argument takes the end number
-level = st.slider("Select the level", 1, 5)
+level = st.slider(".slider: Select the level", 1, 5)
 
 # print the level
 # format() is used to print value 
